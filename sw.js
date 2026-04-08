8cdcef3a011a8c54882c1e4b64c4d366e3a6371f
FadilD0224333 <yougeming010@gmail.com>
2026-04-08 13:31:27 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'daraa-challenge-v1';
+const urlsToCache = [
+  '/Challenge/',
+  '/Challenge/index.html',
+  '/Challenge/manifest.json'
+  // Tambahkan path lain jika ada file CSS/JS/gambar eksternal
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
